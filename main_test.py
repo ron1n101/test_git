4833f66f5f707b7092d0a98d756302bfbd446eeb
Dmitry Golochalov <daticob@gmail.com>
2020-12-05 23:18:52 +0200

    Parcer HW done. 1.parse telemart=wrong,2.parse maybe correct

--- main_test.py
+++ main_test.py
@@ -14,6 +14,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+
 URL = 'https://telemart.ua/processor/intel/'
 HEADERS = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0',
            'accept': '*/*'}
@@ -51,23 +52,61 @@ def parse():
         procc = get_content(html.text)
     else:
         print('Ops')
+# parse() #Запуск тестового парсера
 
-parse()
 
-# def parse_parameters(query: str) -> dict:
-#     return {}
-#
-#
-# def parse_cookies(query: str) -> dict:
-#     return {}
-# if __name__ == '__main__':
-#     # Tests for function "parse_parameters"
-#     assert parse_parameters('') == {}
-#     assert parse_parameters('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-#
-#
-#
-#
-#     # Tests for function "parse_cookies"
-#     assert parse_cookies('') == {}
-#     assert parse_cookies('name=Dima;') == {'name': 'Dima'}
+
+from urllib import parse
+from http.cookies import SimpleCookie
+
+def parse_parameters(url):
+    r = dict(parse.parse_qsl(parse.urlsplit(url).query))
+    return r
+
+def parse_cookies(storage):
+    cookies = SimpleCookie()
+    cookies.load(storage)
+    d_cookies = {}
+    for key, st_cookie in cookies.items():
+        d_cookies[key] = st_cookie.values()
+    return d_cookies
+
+
+if __name__ == '__main__':
+    # Tests for function "parse_parameters"
+    assert parse_parameters('http://example.com/?') == {}
+    assert parse_parameters('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+
+    assert parse_parameters('https://www.youtube.com/watch?v=5qap5aO4i9A') == {'v' : '5qap5aO4i9A'}
+    assert parse_parameters('https://mail.google.com/mail/u/0/?tab=rm#inbox') == {'tab' : 'rm'}
+    assert parse_parameters('https://example.com/path/to/page?name=josh&age=22') == {'name':'josh', 'age': '22'}
+    assert parse_parameters('https://example.com/path/to/page?character=nonproud&name=Irina') == {'character': 'nonproud',
+                                                                                                  'name': 'Irina'}
+
+    assert parse_parameters('http://httpbin.com/page/?input=15&output=24') == {'input': '15', 'output': '24'}
+    assert parse_parameters('http://swap-file.com/path/to/page?file=plshelpmeforthishw&count=404') =={
+        'file': 'plshelpmeforthishw',
+        'count': '404'
+    }
+    assert parse_parameters('http://googlemaps.com/page/?country=Texas&city=Odessa') == {
+        'country' : 'Texas',
+        'city' : 'Odessa'
+    }
+    assert parse_parameters('https://telemart.ua/page/as/prod?proccessor=intel&model=i5') == {
+        'proccessor':'intel',
+        'model':'i5'
+    }
+
+    # Tests for function "parse_cookies"
+    assert parse_cookies('') == {}
+    assert parse_cookies('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookies('v=5qap5aO4i9A;') == {'v': '5qap5aO4i9A'}
+    assert parse_cookies('tab=rm;') == {'tab': 'rm'}
+    assert parse_cookies('name=josh;age=22') == {'name':'josh',
+                                                 'age': '22'}
+    assert parse_cookies('character=nonproud;name=Irina') == {'character': 'nonproud',
+                                                              'name':'Irina'}
+    assert parse_cookies('input=15;output=24') == {'input':'15', 'output':'24'}
+    assert parse_cookies('file=plshelpmeforthishw;count=404') == {'file':'plshelpmeforthishw', 'count':'404'}
+    assert parse_cookies('country=Texas;city=Odessa') == {'country':'Texas', 'city':'Odessa'}
+    assert parse_cookies('proccessor=intel;model=i5') == {'proccessor':'intel', 'model':'i5'}
